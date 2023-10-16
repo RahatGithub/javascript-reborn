db0f324adbe4f6225234ddfe25fc5c5dac591a23
Rahat Ahmed Chowdhury <rahatahmedchowdhuryrac@gmail.com>
2023-10-16 11:55:57 +0600

    exploring_DOM added

--- Basics/test.js
+++ Basics/test.js
@@ -1,11 +1,3 @@
-let cnt = 0
+let fun = () => {let a = 6};
 
-counter = (r) => {
-    for(let i=0; i<r; i++){
-        console.log(this.Array)
-    }
-    return cnt 
-}
-
-x = counter(5)
-console.log(x)
+console.log(a)
